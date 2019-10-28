6561721d3cb3732f9b461b40d70f55db9c8f3fb4
offcn-java <408118064@qq.com>
2019-10-28 16:24:54 +0800

    Initial commit

---
+++ src/main/java/com/offcn/SjtdemoApplication.java
@@ -0,0 +1,13 @@
+package com.offcn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjtdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SjtdemoApplication.class, args);
+    }
+
+}
